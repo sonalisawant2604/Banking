3b39e271fc70298ecff9556863b572313d90603c
RAM <RAM@LAPTOP-I36QRUN5>
2021-06-05 22:19:09 +0530

    second commiyt

--- src/main/java/com/example/shopping/controller/CartItemController.java
+++ src/main/java/com/example/shopping/controller/CartItemController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CartItemController {
 
-	@GetMapping("/")
+	@GetMapping("/demo")
 	public String display()
 	{
 		return "hello";
